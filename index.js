27fbdef85c9346d1f76a2200113df3caf4f2c5d8
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-02 00:18:01 +0330

    feat: add nodemon

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ dotenv.config();
 //
 const {Telegraf} = require("telegraf")
 const bot = new Telegraf(process.env.HTTP_KEY)
-console.log(process.env.HTTP_KEY);
+bot.start(ctx => ctx.reply("سلام! به خفن ترین ربات دنیا خوش اومدین! این ربات برای اینه که بتونید لیست کتاب هایی که در حال خوندن هستید رو ذخیره کنید و اخر هر روز باید اینجا وارد کنید ک چقدر از کتاب هایی ک وارد کردید رو خوندید "))
+
+bot.launch()
 
 
 //ketab khod ra vared konid
