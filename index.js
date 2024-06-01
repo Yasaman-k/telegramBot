bde7c6e7a766802870b1bbce8fefd64c32aeee9a
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-01 23:48:54 +0330

    feat: add gitignore

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hi");
+const telegraf = require
+
+//ketab khod ra vared konid
+// list ketab hay evared shode ra bebinid
+// ketab khod ra entekhab konid
+// benevisid ta koja in ketab ro khondid
