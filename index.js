c6828054e1594b300d8d69a1d696fbe90eaa6ce7
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-07 22:03:09 +0330

    feat: add prettier config

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config();
 //
-const {Telegraf} = require("telegraf")
-const bot = new Telegraf(process.env.HTTP_KEY)
-bot.start(ctx => {
-    console.log(ctx);
-    ctx.reply(`
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.HTTP_KEY);
+bot.start((ctx) => {
+  console.log(ctx);
+  ctx.reply(`
     سلام! به خفن ترین ربات دنیا خوش اومدین 
     برای ایجاد کتاب روی /createBook کلیک کنید
-    `)
-})
-bot.command("createBook",ctx=>{
-ctx.reply("اسم کتاب خود را وارد کنید")
-})
-bot.launch()
-
+    `);
+});
+bot.command('createBook', (ctx) => {
+  ctx.reply('اسم کتاب خود را وارد کنید');
+});
+bot.launch();
 
 //ketab khod ra vared konid
 // list ketab hay evared shode ra bebinid
