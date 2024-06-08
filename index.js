8459edfd91aa021071de22cc517cdefdbe557a76
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-08 13:11:24 +0330

    feat: add middleware

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ dotenv.config();
 //
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.HTTP_KEY);
+// bot.mention('ysmn_kc', (ctx) => ctx.reply('u menstion someone'));
+// bot.on('text', (ctx) => ctx.reply('i love u sara'));
+
+const getUserRole = () => {
+  const roles = ['bronze', 'silver', 'gold'];
+  const index = Math.floor(Math.random() * roles.length);
+  return roles[index];
+};
+
+// middleware
+bot.use((ctx, next) => {
+  ctx.reply('you send a message');
+  const role = getUserRole(ctx.message.from);
+  ctx.state.role = role;
+  next();
+});
+
 bot.start((ctx) => {
-  console.log(ctx);
   ctx.reply(`
     سلام! به خفن ترین ربات دنیا خوش اومدین 
     برای ایجاد کتاب روی /createBook کلیک کنید
     `);
 });
+
 bot.command('createBook', (ctx) => {
   ctx.reply('اسم کتاب خود را وارد کنید');
+  const role = ctx.state.role;
+  ctx.reply(`you ${role}`);
 });
+
 bot.launch();
 
 //ketab khod ra vared konid
