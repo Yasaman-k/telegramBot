9bd963ddfc0cf7ecc4cf906fed69bc4e349b117e
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-10 15:55:20 +0330

    feat: install mongoose

--- app/bot/index.js
+++ app/bot/index.js
@@ -1,10 +1,12 @@
 const { Telegraf } = require('telegraf');
+const LocalSession = require('telegraf-session-local');
 
 let bot;
 
 const startBot = () => {
   bot = new Telegraf(process.env.BOT_TOKEN);
   bot.launch();
+  bot.use(new LocalSession({ database: 'session.json' }));
   bot.start((ctx) => {
     ctx.reply('hi');
   });
