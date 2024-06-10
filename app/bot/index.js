fbb88413d2068b92fb3e7735eaa86eb0508ceccd
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-10 13:45:20 +0330

    feat: add telegraf session local

--- app/bot/index.js
+++ app/bot/index.js
@@ -2,13 +2,12 @@ const { Telegraf } = require('telegraf');
 
 let bot;
 
-function startBot() {
+const startBot = () => {
   bot = new Telegraf(process.env.BOT_TOKEN);
   bot.launch();
-  console.log('pp');
   bot.start((ctx) => {
     ctx.reply('hi');
   });
-}
+};
 
 module.exports.startBot = startBot;
