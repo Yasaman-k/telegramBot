cf16e90e099926df187fc7e7f534413da2dcc780
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2024-06-12 01:54:22 +0330

    feat: install docker and mongodb to run mongopse

--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 const { startBot } = require('./bot');
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://jasmine:ReVlyYsWaWbaSuum@cluster0.j9u8vmg.mongodb.net/';
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 class Application {
   constructor() {
@@ -11,33 +9,20 @@ class Application {
   }
 
   setupMongo() {
-    async function run() {
-      try {
-        console.log('um');
-        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-        await mongoose.connect(uri, clientOptions);
-        await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log('Pinged your deployment. You successfully connected to MongoDB!');
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await mongoose.disconnect();
-      }
+    mongoose
+      .connect('mongodb://127.0.0.1:27017')
+      .then(() => {
+        console.log('db connect');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    async function main() {
+      const res = await mongoose.connect('mongodb://127.0.0.1:27017/');
+
+      // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     }
-    run().catch(console.dir);
-    // mongoose
-    //   .connect(uri, clientOptions)
-    //   .then(() => {
-    //     console.log('db connect');
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-
-    // async function main() {
-    //   const res = await mongoose.connect('mongodb://127.0.0.1:27017/');
-
-    //   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-    // }
   }
 
   configApp() {
