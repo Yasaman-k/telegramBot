32d9573a072d5b7dc419de4dc7a030515e5ebdf9
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-06-16 16:40:58 +0330

    chore: add new messag ehandler

--- app/bot/utils/MessageHandler.js
+++ app/bot/utils/MessageHandler.js
@@ -1,2 +1,3 @@
 module.exports.START_MESSAGE = 'Hi Welcome!';
-module.exports.CATEGORY_LIST = 'please choose one category';
+module.exports.CATEGORY_LIST_MESSAGE = 'please choose one category';
+module.exports.BOOK_LISTـMESSAGE = 'Choose the book to report it';
