f3956cd4ddb4355c626e9eba249808077b70597b
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-06-18 18:18:38 +0330

    feat: add sesions for search

--- app/bot/index.js
+++ app/bot/index.js
@@ -1,8 +1,9 @@
 const { Telegraf } = require('telegraf');
-// const LocalSession = require('telegraf-session-local');
 const { mainButtons } = require('./utils/ButtonManager');
 const KeyboardMiddleware = require('./middleware/Keyboardmiddleware');
 const ActionMiddleware = require('./middleware/ActionMiddleware');
+const SessionMiddleware = require('./middleware/SessionMiddleware');
+const LocalSession = require('telegraf-session-local');
 const { START_MESSAGE } = require('./utils/MessageHandler');
 
 let bot;
@@ -10,9 +11,10 @@ let bot;
 const startBot = () => {
   bot = new Telegraf(process.env.BOT_TOKEN);
   bot.launch();
-  //  bot.use(new LocalSession({ database: 'session.json' }));
+  bot.use(new LocalSession({ database: 'session.json' }));
   // bot.use is used for middlewares
   bot.use(KeyboardMiddleware);
+  bot.use(SessionMiddleware);
   bot.use(ActionMiddleware);
   bot.start((ctx) => {
     ctx.reply(START_MESSAGE, mainButtons);
--- app/bot/middleware/SessionMiddleware.js
+++ app/bot/middleware/SessionMiddleware.js
@@ -22,11 +22,10 @@ const EventListener = {
   [STATE_LIST.SEARCH]: async (ctx, next) => {
     if (ctx.message) {
       ctx.session.state = undefined;
-      //   console.log(ctx.message);
-      //   const cat = await Category.find({ title: ctx.message.text });
-      //   console.log(cat, 'ss');
-      //   console.log();
+      const cat = await Category.findOne({ title: ctx.message.text });
+      const book = await Book.find({ cat: cat._id });
       ctx.reply(`you are looking for _${ctx.message.text}_`, { parse_mode: 'Markdown' });
+      ctx.reply('these are your books related to this caregory', booksListButtons(book));
     } else {
       next();
     }
