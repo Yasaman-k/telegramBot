ba35400a9d7d067d3573b2d91d1c1a76b4ffb32a
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-06-23 14:36:50 +0330

    feat: add await

--- app/bot/middleware/ActionMiddleware.js
+++ app/bot/middleware/ActionMiddleware.js
@@ -42,7 +42,7 @@ const EventListener = {
       // ctx.replyWithPhoto({
       //   url: 'https://dkstatics-public.digikala.com/digikala-products/9257abcf926b66bfdfdcf550fa1e7db82f281628_1595165673.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90',
       // });
-      ctx.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
+      await ctx.telegram.sendChatAction(ctx.chat.id, 'upload_photo');
       await ctx.replyWithPhoto('AgACAgQAAxkBAAIDXGZ0ItuVib4DRXwWAaV7hNPAejHIAAJxwjEbqAABoFMUcH22TuPhVAEAAwIAA20AAzUE');
     } else {
       ctx.reply('book not found');
